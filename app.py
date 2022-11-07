0428f15046199ad10374eee2aa959f4065633942
baig8 <75277853+baig8@users.noreply.github.com>
2022-11-07 15:12:39 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,39 +9,169 @@ from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 import csv
 from flask import Flask, request, render_template
-
-
-
-# import joblib
-
+#-----------------------------------------------------
 
 app = Flask(__name__)
+#
 model = pickle.load(open('model_new.pkl', 'rb'))
-
-
-# model = XGBClassifier.Booster({'nthread':4})
-# model.load_model('new.pkl')
-
+#
+# #----------------------------------------------------
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+
+@app.route('/home', methods=['POST'])
+def home():
+
+
+    name = request.args.get("name")
+
+    with open('newfile.csv', newline='') as csv_file:
+        data = csv.DictReader(csv_file)
+        for row in data:
+            # print(row)
+            # print(row['perimeter'])
+            # using key to store the values in a variable and then send to a list
+
+            radius = row['radius']
+            texture = row['texture']
+            perimeter = row['perimeter']
+            area = row['area']
+            smoothness = row['smoothness']
+            concavity = row['concavity']
+            concavePoints = row['concave_points']
+            symmetry = row['symmetry']
+            fractalDimension = row['fractal_dimension']
+            worstRadius = row['worst_radius']
+            worstTexture = row['worst_texture']
+            worstPerimeter = row['worst_perimeter']
+            # print(worstPerimeter)
+            worstArea = row['worst_area']
+            worstSmoothness = row['worst_smoothness']
+            worstCompactness = row['worst_compactness']
+            worstConcavity = row['worst_concavity']
+            worstConcavePoints = row['worst_concave_points']
+            worstSymmetry = row['worst_symmetry']
+            worstFractal_dimension = row['worst_fractal_dimension']
+            meanRadius = row['mean_radius']
+            meanTexture = row['mean_texture']
+            meanPerimeter = row['mean_perimeter']
+            meanArea = row['mean_area']
+            meanSmoothness = row['mean_smoothness']
+            meanCompactness = row['mean_compactness']
+            meanConcavity = row['mean_concavity']
+            meanConcavePoints = row['mean_concave_points']
+            meanSymmetry = row['mean_symmetry']
+
+            meanFractalDimension = row['mean_fractal_dimension']
+            meanFractalerror = row['mean_fractal_dimension']
+
+            # print(meanFractalDimension)
+            auto = [
+
+                radius,
+                texture,
+                perimeter,
+                area,
+                smoothness,
+                concavity,
+                concavePoints,
+                symmetry,
+                fractalDimension,
+                worstRadius,
+                worstTexture,
+                worstPerimeter,
+                # print(worstPerimeter)
+                worstArea,
+                worstSmoothness,
+                worstCompactness,
+                worstConcavity,
+                worstConcavePoints,
+                worstSymmetry,
+                worstFractal_dimension,
+                meanRadius,
+                meanTexture,
+                meanPerimeter,
+                meanArea,
+                meanSmoothness,
+                meanCompactness,
+                meanConcavity,
+                meanConcavePoints,
+                meanSymmetry,
+                meanFractalDimension,
+                meanFractalerror
+
+            ]
+            # print(auto)
+            # print(auto[0])
+
+#            feature_value = [float (x) for x in request.form.values([np.array(auto)])]
+            feature_value = [np.array(auto)]
+
+
+            # print(feature_value)
+
+            features_name = ['radius',
+                             'texture',
+                             'perimeter',
+                             'area',
+                             'smoothness',
+                             'concavity',
+                             'concavePoints',
+                             'symmetry',
+                             'fractalDimension',
+                             'worstRadius',
+                             'worstTexture',
+                             'worstPerimeter',
+                             # print(worstPerimeter)
+                             'worstArea',
+                             'worstSmoothness',
+                             'worstCompactness',
+                             'worstConcavity',
+                             'worstConcavePoints',
+                             'worstSymmetry',
+                             'worstFractal_dimension',
+                             'meanRadius',
+                             'meanTexture',
+                             'meanPerimeter',
+                             'meanArea',
+                             'meanSmoothness',
+                             'meanCompactness',
+                             'meanConcavity',
+                             'meanConcavePoints',
+                             'meanSymmetry',
+                             'meanFractalDimension',
+                             'mean_fractalerror'
+                             ]
+
+            # print(features_name)
+
+            df = pd.DataFrame(feature_value, columns=features_name)
+            # print(df)
+
+            output = model.predict(df)
+
+            # THis is the prediction output
+            # print(output)
+
+            if output == 0:
+                res_val = "---^--^----^-***Cancer Detected***------------"
+            else:
+                res_val = "---***-No Cancer Detected-***---"
+
+         #   new = res_val
+
+        #     print(res_val)
+
+            return render_template('index.html', name =res_val)
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    #with open('newfile.csv',newline='') as csv_file:
-     #   data = csv.DictReader(csv_file)
-
-      #  for col in data:
-#  #   print(col('radius'))
-     #       data = [float(x) for x in request.form.values()]
-
     input_features = [float(x) for x in request.form.values()]
     features_value = [np.array(input_features)]
-#        feature_vls = [np.array(data)]
-
-
     features_name = ['mean radius', 'mean texture', 'mean perimeter', 'mean area',
                      'mean smoothness', 'mean compactness', 'mean concavity',
                      'mean concave points', 'mean symmetry', 'mean fractal dimension',
@@ -53,9 +183,7 @@ def predict():
                      'worst concave points', 'worst symmetry', 'worst fractal dimension']
 
     df = pd.DataFrame(features_value, columns=features_name)
-#    df = pd.DataFrame(features_value, columns=features_name)
     output = model.predict(df)
-
     if output == 0:
         res_val = "** Cancer Detected **"
     else:
@@ -63,10 +191,12 @@ def predict():
 
     return render_template('index.html', prediction_text='In Patient data {}'.format(res_val))
 
-if __name__ == "__main__":
-    #     app.debug = True
-    app.run()
 
 
 
 
+
+
+if __name__ == '__main__':
+   #home()
+   app.run()
